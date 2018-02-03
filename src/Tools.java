07d8d693e4ee0e62b13dc2d94d28bbfd5e6bd496
yodasnow <oliverode@gmail.com>
2018-02-03 16:13:30 -0500

    Computer change

--- src/Tools.java
+++ src/Tools.java
@@ -1,11 +1,10 @@
 import java.util.Random;
 public class Tools {
     /**
-     * This function will return a sorted version of a list using Bubble Sort
-     * @return
+     * This function will sort a list using Bubble Sort (left to right normal method)
+     * @param lst
      */
-    public static int[] bubbleSort(int[] lst){
-        long startTime = System.currentTimeMillis();
+    public static void bubbleSort(int[] lst){
         int holder = 0;
         for(int k = 0; k < lst.length-1; k++){
             for(int i = 0; i < lst.length-1; i++){
@@ -16,10 +15,35 @@ public class Tools {
                 }
             }
         }
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime-startTime);
-        return lst;
     }
+
+    /**
+     * This function will sort a list using Selection Sort (right to left)
+     * @param lst
+     */
+    public static void selectionSort(int[] lst){
+        for(int i = lst.length-1; i >= 0; i--){
+            int largestIndex = 0;
+            int max = lst[0];
+            for(int j = 0; j <= i; j++){
+                if(lst[j] >= max){
+                    max = lst[j];
+                    largestIndex = j;
+                }
+            }
+            int temp = lst[i];
+            lst[i] = max;
+            lst[largestIndex] = temp;
+        }
+    }
+
+    /**
+     * This function will randomly make a list with the 3 specified values
+     * @param length
+     * @param min
+     * @param max
+     * @return
+     */
     public static int[] randomListGenerator(int length, int min, int max){
         Random rand = new Random();
         int[] lst = new int[length];
--- src/ToolsTester.java
+++ src/ToolsTester.java
@@ -1,9 +1,32 @@
-import java.util.Arrays;
 public class ToolsTester {
     public static void main(String[] args) {
-        for(int i = 0; i < 100; i++){
-            Arrays.toString(Tools.bubbleSort(Tools.randomListGenerator(100000, 0, 1000)));
-            //System.out.println(Arrays.toString(Tools.bubbleSort(Tools.randomListGenerator(1000, 0, 1000))));
+        int[] lengths = {10, 100, 1000, 10000, 100000, 1000000, 1000000000};
+        int[] possibleNumbers = {10, 100, 1000, 10000, 100000, 1000000, 1000000000};
+        int numOfTestCases = 20;
+        long[] averageHolder = new long[numOfTestCases];
+        int average = 0;
+        for(int i = 0; i < lengths.length-2; i ++){ //Different length test cases and max value
+            System.out.println("Doing array length " + lengths[i] + " test cases.");
+            for(int j = 0; j < 1; j++){ //Sorting method
+                System.out.println("Doing sorting method " + j + ".");
+                for(int k = 0; k < numOfTestCases; k++){ // Amount of runs
+                    long startTime = System.currentTimeMillis();
+                    int nums[] = new int[lengths[i]];
+                    nums = Tools.randomListGenerator(lengths[i], 0, possibleNumbers[i]);
+
+                    if(j == 0) Tools.bubbleSort(nums);
+
+                    long endTime = System.currentTimeMillis();
+                    averageHolder[k] = endTime-startTime;
+                }
+                for(int k = 0; k < numOfTestCases; k++){
+                    average += averageHolder[k];
+                }
+                average /= numOfTestCases;
+                System.out.println(average);
+                average = 0;
+                averageHolder = new long[numOfTestCases];
+            }
         }
     }
 }
